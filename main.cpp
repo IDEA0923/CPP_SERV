5f79c75b3b480d38663efd7e78e2ea2fb5ee349d
IDEA <jjggsfx@gmail.com>
2025-04-18 19:15:30 +1000

    done

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <unistd.h>
 ///////////////////////////////////////////////////////
 
-#define PORT 80 
+#define PORT 5500
 
 
 
@@ -19,6 +19,7 @@ using namespace std;
 void setp(int& sock , struct sockaddr_in& adress ){
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock == 0){
+        cout<<"BAD SOCK"<<endl;
         return ;
     }
     //
@@ -27,14 +28,21 @@ void setp(int& sock , struct sockaddr_in& adress ){
     adress.sin_addr.s_addr = INADDR_ANY;
     adress.sin_port = htons(PORT);
 
-    if(bind(sock , (struct sockaddr *)&adress, sizeof(adress)) < 0){
-        return ;
-    }
+    //if(
+    bind(sock , (struct sockaddr *)&adress, sizeof(adress));
+//){
+    //     cout<<"BAD BIND"<<endl;
+    //     return ;
+    // }
+
     if ( listen(sock, SOMAXCONN) < 0){
+        cout<<"BAD LISTEN"<<endl;
         return;
     }
 }
 void func(struct sockaddr_in& client_addr, int& new_sd){
+    cout<<"USER"<<endl;
+    
     // char memory[1024] = { 0 };
     // int n;
 
@@ -55,18 +63,13 @@ int main(){
     struct sockaddr_in client_addr;
     socklen_t length = sizeof(sockaddr_in);
     int new_sd;
-
-            
+    cout<<"START"<<endl;
     while ((new_sd = accept(sock, (sockaddr*)&client_addr, (socklen_t*)&length)) > 0)
     {
         cout << "Client Accepted" << endl;
-        thread t(func, client_addr, ref(new_sd));
+        thread t(func, ref(client_addr), ref(new_sd));
         poolofthread.push_back(move(t));
     }
     return 0;
-
-
-
-
 
 }
